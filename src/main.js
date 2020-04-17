194123214f9490aec3bda9035aa3769e85409123
Jordi P. Bou <jperez@serialnet.net>
2020-04-17 18:47:27 +0200

    Deleted things not used anymore. Started working on RxMusic, added music notes, pitch classes and intervals. Started work on patterns.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import App from './App.svelte';
 import './main.css';
 
-export * as Utils from './utils.js'
-//export * as JSorth from './jsorth.js'
 export * as RxMidi from './rxmidi.js'
-//export * as RxMidi from './reactivemidi.js'
+export * as RxMusic from './rxmusic.js'
 export * as LinnStrumentJS from './linnstrument.js'
-export * as RxSequencer from './rxsequencer.js'
 export * as Effector from 'effector'
 
 export const efimera = new App({
--- src/rxmidi.js
+++ src/rxmidi.js
@@ -27,7 +27,7 @@ export let logPorts =
 // input that includes the argument in its name
 export let input = n => [...midiAccess.inputs].map( ([k, v]) => v )
 											  .filter( ({ name }) => name.includes(n) )
-											  .map( v => fromEvent(v, 'midimessage') )[0]
+											  .map( v => fromEvent(v, 'midimessage').pipe(asMIDIMessage()) )[0]
 
 // Returns a function that receives MIDI events and sends them
 // to first output that includes the argument in its name.
@@ -38,9 +38,9 @@ export let output = n => [...midiAccess.outputs].map( ([k, v]) => v )
 												.map( v => { v.open(); return v; } )
 												.map( v => (x) => {
 																if (x.type === 'midimessage') {
-																	v.send(x.data)
+																	v.send(x.data, x.timeStamp)
 																} else if (x instanceof Observable) {
-																	x.subscribe(y => v.send(y.data))
+																	x.subscribe(y => v.send(y.data, y.timeStamp))
 																} else {
 																	// Just an array of data
 																	v.send(x)
@@ -49,62 +49,70 @@ export let output = n => [...midiAccess.outputs].map( ([k, v]) => v )
 
 // ==== MIDI Messages creation ====
 // Converts one MIDI Message byte array to a MIDIMessageEvent
+// Delay indicates the amount of time we will to wait until
+// send it (in ms).
 export let msg = 
-	data => { return { type: 'midimessage', timeStamp: performance.now(), deltaTime: 0.0, data: [ ...data ]}}
+	(data, delay = 0) => { 
+		return { 
+			type: 'midimessage', 
+			timeStamp: performance.now() + delay,
+			deltaTime: 0.0, 
+			data: [ ...data ]
+		}}
 export let copy = 
 	mm => { return { type: 'midimessage', timeStamp: mm.timeStamp, deltaTime: mm.deltaTime, data: [ ...mm.data ]}}
 // ---- MIDI Messages definition ----
 // --- Channel Voice messages generation ---
-export let on =	(n, v = 96, ch = 0) => msg([144 + ch, n, v])
-export let off = (n, v = 96, ch = 0) => msg([128 + ch, n, v])
-export let pp = (n, v = 96, ch = 0) => msg([160 + ch, n, v])
-export let cc =	(c, v, ch = 0) => msg([176 + ch, c, v])
-export let pc = (p, ch = 0) => msg([192 + ch, p])
-export let cp = (v, ch = 0) => msg([208 + ch, v])
-export let pb =	(v, ch = 0) => msg([224 + ch, v % 128, v >> 7])
+export let on =	(n, v = 96, ch = 0, delay = 0) => msg([144 + ch, n, v], delay)
+export let off = (n, v = 96, ch = 0, delay = 0) => msg([128 + ch, n, v], delay)
+export let pp = (n, v = 96, ch = 0, delay = 0) => msg([160 + ch, n, v], delay)
+export let cc =	(c, v, ch = 0, delay = 0) => msg([176 + ch, c, v], delay)
+export let pc = (p, ch = 0, delay = 0) => msg([192 + ch, p], delay)
+export let cp = (v, ch = 0, delay = 0) => msg([208 + ch, v], delay)
+export let pb =	(v, ch = 0, delay = 0) => msg([224 + ch, v % 128, v >> 7], delay)
 export let rpn = 
-	(n, v, ch = 0) => from([
-		cc(101, n >> 7, ch),
-		cc(100, n % 128, ch), 
-		cc(6, v >> 7, ch),
-		cc(38, v % 128, ch),
-		cc(101, 127, ch),
-		cc(100, 127, ch)
+	(n, v, ch = 0, delay = 0) => from([
+		cc(101, n >> 7, ch, delay),
+		cc(100, n % 128, ch, delay), 
+		cc(6, v >> 7, ch, delay),
+		cc(38, v % 128, ch, delay),
+		cc(101, 127, ch, delay),
+		cc(100, 127, ch, delay)
 	])
 export let nrpn =
-	(n, v, ch = 0) => from([
-		cc(99, n >> 7, ch),
-		cc(98, n % 128, ch),
-		cc(6, v >> 7, ch),
-		cc(38, v % 128, ch),
-		cc(101, 127, ch),
-		cc(100, 127, ch)
+	(n, v, ch = 0, delay = 0) => from([
+		cc(99, n >> 7, ch, delay),
+		cc(98, n % 128, ch, delay),
+		cc(6, v >> 7, ch, delay),
+		cc(38, v % 128, ch, delay),
+		cc(101, 127, ch, delay),
+		cc(100, 127, ch, delay)
 	])
 // ---- System common messages generation ----
-export let syx = (b) => msg([240, ...b, 247])
-export let tc = (t, v) => msg([241, (t << 4) + v])
-export let spp = (b) => msg([242, b % 128, b >> 7])
-export let s = (s) => msg([243, s])
-export let tun = () => msg([246])
+export let syx = (b, delay = 0) => msg([240, ...b, 247], delay)
+export let tc = (t, v, delay = 0) => msg([241, (t << 4) + v], delay)
+export let spp = (b, delay = 0) => msg([242, b % 128, b >> 7], delay)
+export let s = (s, delay = 0) => msg([243, s], delay)
+export let tun = (delay = 0) => msg([246], delay)
 // ---- System real time messages generation ----
 export let clock =
 	// TODO: Send 2 beats of MIDI Timing Clock for indicated BPM (1-999)
 	bpm => []
-export let mc = () => msg([248])
-export let start = () => msg([250])
-export let cont = () => msg([251])
-export let stop = () => msg([252])
-export let as = () => msg([254])
-export let rst = () => msg([255])
+export let mc = (delay = 0) => msg([248], delay)
+export let start = (delay = 0) => msg([250], delay)
+export let cont = (delay = 0) => msg([251], delay)
+export let stop = (delay = 0) => msg([252], delay)
+export let as = (delay = 0) => msg([254], delay)
+export let rst = (delay = 0) => msg([255], delay)
 export let panic = 
-	() => {
+	(delay = 0) => {
 		let panic_msgs = []
 		for (let ch = 0; ch < 16; ch++) {
-			panic_msgs.push(cc(64, 0, ch))
-			panic_msgs.push(cc(120, 0, ch))
-			panic_msgs.push(cc(123, 0, ch))
+			panic_msgs.push(cc(64, 0, ch, delay))
+			panic_msgs.push(cc(120, 0, ch, delay))
+			panic_msgs.push(cc(123, 0, ch, delay))
 			for (let n = 0; n < 128; n++) {
-				panic_msgs.push(off(n, 0, ch))
+				panic_msgs.push(off(n, 0, ch, delay))
 			}
 		}
 
@@ -165,6 +173,10 @@ export let mapChannel =	(chin, chout) => map(d => isOnChannel(d, chin) ? changeT
 export let mapChannels = (chsin, chsout) => mergeMap(d => isOnChannels(d, chsin) ? 
 														  from(chsout.map(ch => changeToChannel(copy(d), ch)))
 														  : of(d))
+export let now = () => performance.now()
+export let at = (t) => map(d => { d.timeStamp = t; return d; })
+export let delay = (delay) => map(d => { d.timeStamp = d.timeStamp + delay; return d; })
+export let asMIDIMessage = () => map(d => copy(d))
 // ---- Controller mapping ----
 // TODO: Add transformation curves to values
 export let mapController =
