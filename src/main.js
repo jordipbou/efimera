dd473781b50a7bd92562dd3c86be297a291e2df8
Jordi P. Bou <jperez@serialnet.net>
2020-04-18 20:13:04 +0200

    Deleted Effector, it can be easily recreated with RxJS alone.

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ export * as RxMidi from './rxmidi.js'
 export * as RxMusic from './rxmusic.js'
 export * as RxMusic_Defs from './rxmusic_defs.js'
 export * as LinnStrumentJS from './linnstrument.js'
-export * as Effector from 'effector'
 
 export const efimera = new App({
 	target: document.body,
