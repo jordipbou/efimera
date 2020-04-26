8bfdc3897aa862488e74eba193ad976ceb2e25d0
Jordi P. Bou <jperez@serialnet.net>
2020-04-27 00:31:58 +0200

    Changed clock implementation to allow working with interval windows and midi beat clocks simultaneously

--- src/rxmidi.js
+++ src/rxmidi.js
@@ -37,8 +37,13 @@ export let output = n => [...midiAccess.outputs].map( ([k, v]) => v )
 																	v.send(x.data, x.timeStamp)
 																} else if (x.type === 'metaevent') {
 																	// Do nothing with that message
-																} else if (x instanceof Observable) {
-																	x.subscribe(y => v.send(y.data, y.timeStamp))
+																} else if ('_subscribe' in x) {
+																	// An observable, subscribe to it
+																	x.subscribe(y => {
+																		if (y.type === 'midimessage') {
+																			v.send(y.data, y.timeStamp)
+																		}
+																	})
 																} else {
 																	// Just an array of data
 																	v.send(x)
@@ -50,76 +55,94 @@ export let output = n => [...midiAccess.outputs].map( ([k, v]) => v )
 // Delay indicates the amount of time we will to wait until
 // send it (in ms).
 export let msg = 
-	(data, delay = 0) => { 
+	(data, delay = 0, deltaTime = 0.0, timeStamp = performance.now()) => { 
 		return { 
 			type: 'midimessage', 
-			timeStamp: performance.now() + delay,
-			deltaTime: 0.0, 
+			timeStamp: timeStamp + delay,
+			deltaTime: deltaTime, 
 			data: [ ...data ]
 		}}
 export let copy = 
-	mm => { return { type: 'midimessage', timeStamp: mm.timeStamp, deltaTime: mm.deltaTime, data: [ ...mm.data ]}}
-// Create just one message with all observables messages, useful to send to output without calling lots
-// of times to output.send
-export let observableToMsgArray =
-	o => {
-		let msg = []
-		o.
+	(mm, timeStamp = null) => { 
+		return { 
+			type: mm.type,
+			timeStamp: timeStamp === null ? mm.timeStamp : timeStamp, 
+			deltaTime: mm.deltaTime, 
+			data: mm.data instanceof Object ? [ ...mm.data ] : [ mm.data ]
+		}
 	}
 // ---- MIDI Messages definition ----
 // --- Channel Voice messages generation ---
-export let on =	(n, v = 96, ch = 0, delay = 0) => msg([144 + ch, n, v], delay)
-export let off = (n, v = 96, ch = 0, delay = 0) => msg([128 + ch, n, v], delay)
-export let pp = (n, v = 96, ch = 0, delay = 0) => msg([160 + ch, n, v], delay)
-export let cc =	(c, v, ch = 0, delay = 0) => msg([176 + ch, c, v], delay)
-export let pc = (p, ch = 0, delay = 0) => msg([192 + ch, p], delay)
-export let cp = (v, ch = 0, delay = 0) => msg([208 + ch, v], delay)
-export let pb =	(v, ch = 0, delay = 0) => msg([224 + ch, v % 128, v >> 7], delay)
+export let on =	(n, v = 96, ch = 0, delay = 0, delta = 0) => msg([144 + ch, n, v], delay, delta)
+export let off = (n, v = 96, ch = 0, delay = 0, delta = 0) => msg([128 + ch, n, v], delay, delta)
+export let pp = (n, v = 96, ch = 0, delay = 0, delta = 0) => msg([160 + ch, n, v], delay, delta)
+export let cc =	(c, v, ch = 0, delay = 0, delta = 0) => msg([176 + ch, c, v], delay, delta)
+export let pc = (p, ch = 0, delay = 0, delta = 0) => msg([192 + ch, p], delay, delta)
+export let cp = (v, ch = 0, delay = 0, delta = 0) => msg([208 + ch, v], delay, delta)
+export let pb =	(v, ch = 0, delay = 0, delta = 0) => msg([224 + ch, v % 128, v >> 7], delay, delta)
 export let rpn = 
-	(n, v, ch = 0, delay = 0) => from([
-		cc(101, n >> 7, ch, delay),
-		cc(100, n % 128, ch, delay), 
-		cc(6, v >> 7, ch, delay),
-		cc(38, v % 128, ch, delay),
-		cc(101, 127, ch, delay),
-		cc(100, 127, ch, delay)
+	(n, v, ch = 0, delay = 0, delta = 0) => from([
+		cc(101, n >> 7, ch, delay, delta),
+		cc(100, n % 128, ch, delay, delta), 
+		cc(6, v >> 7, ch, delay, delta),
+		cc(38, v % 128, ch, delay, delta),
+		cc(101, 127, ch, delay, delta),
+		cc(100, 127, ch, delay, delta)
 	])
 export let nrpn =
-	(n, v, ch = 0, delay = 0) => from([
-		cc(99, n >> 7, ch, delay),
-		cc(98, n % 128, ch, delay),
-		cc(6, v >> 7, ch, delay),
-		cc(38, v % 128, ch, delay),
-		cc(101, 127, ch, delay),
+	(n, v, ch = 0, delay = 0, delta = 0) => from([
+		cc(99, n >> 7, ch, delay, delta),
+		cc(98, n % 128, ch, delay, delta),
+		cc(6, v >> 7, ch, delay, delta),
+		cc(38, v % 128, ch, delay, delta),
+		cc(101, 127, ch, delay, delta),
 		cc(100, 127, ch, delay)
 	])
 // ---- System common messages generation ----
-export let syx = (b, delay = 0) => msg([240, ...b, 247], delay)
-export let tc = (t, v, delay = 0) => msg([241, (t << 4) + v], delay)
-export let spp = (b, delay = 0) => msg([242, b % 128, b >> 7], delay)
-export let s = (s, delay = 0) => msg([243, s], delay)
-export let tun = (delay = 0) => msg([246], delay)
+export let syx = (b, delay = 0, delta = 0) => msg([240, ...b, 247], delay, delta)
+export let tc = (t, v, delay = 0, delta = 0) => msg([241, (t << 4) + v], delay, delta)
+export let spp = (b, delay = 0, delta = 0) => msg([242, b % 128, b >> 7], delay, delta)
+export let s = (s, delay = 0, delta = 0) => msg([243, s], delay, delta)
+export let tun = (delay = 0, delta = 0) => msg([246], delay, delta)
 // ---- System real time messages generation ----
+// Implements a MIDI clock based on the technique explained on:
+// https://www.html5rocks.com/en/tutorials/audio/scheduling/
+// but, as we're working with midi, all the beats (ticks) of a
+// window are sent as an observable with their correct timings.
 export let clock = 
-	(bpm, beats = 0, ppqn = 24) => 
-		interval((1000 / (bpm / 60)) / ppqn).pipe(beats > 0 ? 
-													pipe(take(beats * ppqn), mapTo(mc())) 
-												 	: mapTo(mc()))
-export let mc = (delay = 0) => msg([248], delay)
-export let start = (delay = 0) => msg([250], delay)
-export let cont = (delay = 0) => msg([251], delay)
-export let stop = (delay = 0) => msg([252], delay)
-export let as = (delay = 0) => msg([254], delay)
-export let rst = (delay = 0) => msg([255], delay)
+	(bpm, time_division = 24, resolution = 25, window = 100) => 
+		interval(resolution).pipe(
+			scan(([last_tick_time, bpm, time_division, window, observable], v) => {
+				let ms_per_tick = 60000 / (bpm * time_division)
+				let next_tick_time = last_tick_time + ms_per_tick
+				let start = performance.now()
+				let end = start + window
+				let beats = []
+				while (next_tick_time < end) {
+					beats.push(mc(0, 0, next_tick_time))
+					next_tick_time = next_tick_time + ms_per_tick
+				}
+				
+				return [next_tick_time - ms_per_tick, bpm, time_division, window, from(beats)]
+			}, [performance.now(), bpm, time_division, window, rx.never()]),
+			switchMap(([l, b, t, w, o]) => o)
+		)
+
+export let mc = (delay = 0, delta = 0, timeStamp = performance.now()) => msg([248], delay, delta, timeStamp)
+export let start = (delay = 0, delta = 0) => msg([250], delay, delta)
+export let cont = (delay = 0, delta = 0) => msg([251], delay, delta)
+export let stop = (delay = 0, delta = 0) => msg([252], delay, delta)
+export let as = (delay = 0, delta = 0) => msg([254], delay, delta)
+export let rst = (delay = 0, delta = 0) => msg([255], delay, delta)
 export let panic = 
-	(delay = 0) => {
+	(delay = 0, delta = 0) => {
 		let panic_msgs = []
 		for (let ch = 0; ch < 16; ch++) {
-			panic_msgs.push(cc(64, 0, ch, delay))
-			panic_msgs.push(cc(120, 0, ch, delay))
-			panic_msgs.push(cc(123, 0, ch, delay))
+			panic_msgs.push(cc(64, 0, ch, delay, delta))
+			panic_msgs.push(cc(120, 0, ch, delay, delta))
+			panic_msgs.push(cc(123, 0, ch, delay, delta))
 			for (let n = 0; n < 128; n++) {
-				panic_msgs.push(off(n, 0, ch, delay))
+				panic_msgs.push(off(n, 0, ch, delay, delta))
 			}
 		}
 
@@ -212,6 +235,7 @@ export let loadMidiFile =
 					// with rest of library
 					for (let t of o.track) {
 						for (let e of t.event) {
+							e.timeStamp = 0
 							if (e.type > 7 && e.type < 14) {
 								if (e.data instanceof Array) {
 									e.data = [(e.type << 4) + e.channel, ...e.data]
