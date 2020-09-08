e085924e15e7094a632a158bc8c322ba0c8a3a60
Jordi P. Bou <jperez@serialnet.net>
2020-09-08 18:36:11 +0200

    Added 'export as text' and 'export as text and link' to be able to 'embed' Efimera code in documentations

--- src/ExportJSONView.js
+++ src/ExportJSONView.js
@@ -1,6 +1,7 @@
 import { dispatch, html, render } from 'hybrids'
 import { ref, setOnCloseListener } from './Utils.js'
 import dialogPolyfill from 'dialog-polyfill'
+import { addIndex, join, map, prop } from 'ramda'
 
 export const showExportDialog = (json) => (host) => {
   setOnCloseListener (host) (host.dialog)
@@ -13,9 +14,35 @@ const copyToClipboard = (host, evt) => {
   host.dialog.close ()
 }
 
+const formatLink = (json) =>
+  'https://jordipbou.github.com/efimera/?json=' + encodeURI (json)
+
 const copyLinkToClipboard = (host, evt) => {
-  navigator.clipboard.writeText ('https://jordipbou.github.com/efimera/?json=' + encodeURI (host.json))
+  navigator.clipboard.writeText (formatLink (host.json))
   host.dialog.close ()
+}
+
+const formatText = (blocks) =>
+    join ('\n')
+         (map (join ('\n'))
+           (map 
+             (addIndex (map)
+                       ((l, i) => i === 0 ? '> ' + l : '… ' + l))
+             (map (prop ('lines')) (blocks))))
+
+const copyTextToClipboard = (host, evt) => {
+  let object = JSON.parse (host.json)
+  let text = formatText (object.blocks)
+  navigator.clipboard.writeText (text)
+}
+
+const copyTextAndLinkToClipboard = (host, evt) => {
+  let object = JSON.parse (host.json)
+  let text = formatText (object.blocks)
+  let link = formatLink (host.json)
+
+  navigator.clipboard.writeText (
+    text + '\n[Load on Efimera](' + link + ')')
 }
 
 export const ExportJSONView = {
@@ -33,6 +60,8 @@ export const ExportJSONView = {
         <div>
           <button onclick=${ copyToClipboard }>Copy</button>
           <button onclick=${ copyLinkToClipboard }>Copy link</button>
+          <button onclick=${ copyTextToClipboard }>Only text</button>
+          <button onclick=${ copyTextAndLinkToClipboard }>Text and link</button>
         </div>
       </div>
       <div class="json-export-preview">${ json }</div>
